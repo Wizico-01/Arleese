070aff84bab06d5e0f24c9ca534cfe9309499ec2
Wisdom Godswill <wisdomgodswill15@gmail.com>
2026-05-26 23:26:31 +0100

    edited the app

--- src/components/modals/ListingModal.jsx
+++ src/components/modals/ListingModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { supabase } from '../lib/supabase' // Added your Supabase client connection
+import { supabase } from '../../lib/supabase'
 import { Ic, I } from '../Icons'
 import { Btn, Badge } from '../UI'
 
